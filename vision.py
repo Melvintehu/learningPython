7db51971b76cd7f4965b1acf1c796db3770325c4
Melvin Tehubijuluw <melvindrachten@hotmail.com>
2019-07-31 16:48:52 +0200

    bot

--- vision.py
+++ vision.py
@@ -7,11 +7,12 @@ import cv2
 import pyautogui
 from pytesseract import Output
 import time
+import numpy as np
 
 width, heigth = pyautogui.size()
 
 def makeCoordinateImage():
-    coordinateImg = pyautogui.screenshot(region=(0, 25, 140, 20))
+    coordinateImg = pyautogui.screenshot(region=(0, 25, 160, 25))
     coordinateImg.save(r"C:/Users/Melvin Tehubijuluw/Desktop/shadowbot/coordinates.png")
     return coordinateImg
 
@@ -26,74 +27,82 @@ def getCurrentCoordinates():
             x = x.replace(',', '') 
             x = x.replace(')', '')        
             x = x.replace('(', '') 
+            x = x.replace('[', '')
+            x = x.replace(']', '')
+            x = x.replace('-', '')
             coordinates.append(x)
 
-    return coordinates
+    return normalizeCoordinates(coordinates)
+
+def normalizeCoordinates(coordinates):
+    normalizedCoordinates = []
+    if len(coordinates) >= 2:
+        normalizedCoordinates.append(coordinates[0])
+        normalizedCoordinates.append(coordinates[1])
+    else:
+        normalizedCoordinates.append("")
+        normalizedCoordinates.append("")
+
+    for index, value in enumerate(normalizedCoordinates):
+        tempValue = value.replace('.', '')
+        if len(tempValue) == 4 and tempValue.isdigit():
+            normalizedCoordinates[index] = float(value)
+        else:
+            normalizedCoordinates[index] = ""
+
+    return normalizedCoordinates
 
 
-def moveTo(x, y):
+def moveTo(desX, desY):
     coordinates = getCurrentCoordinates()
-    pyautogui.keyDown('w')
+    
 
     while True:
-        print(isfloat(coordinates[0]), coordinates[0])
-        print(isfloat(coordinates[1]), coordinates[1])
-
-        if isfloat(coordinates[0]) and isfloat(coordinates[1]):
-            print(coordinates[0], coordinates[1])
-            moveToDestination(coordinates[0], coordinates[1], x, y)
-
-        coordinates = getCurrentCoordinates()
-
-def destinationReached(x, y, destinationX, destinationY):
-    return x == destinationX and y == destinationY
-
-
-def isfloat(value):
-    try:
-        float(value)
-        return value.replace('.', '').replace('-', '').replace('[', '').isdigit() and len(value) == 5
-    except ValueError:
-        return False
-
-
-def moveToDestination(x, y, destinationX, destinationY):
-    if isfloat(x) and isfloat(y):
-        coordinates = getCurrentCoordinates()
-        newX = coordinates[0]
-        newY = coordinates[1]
+        print(coordinates)
+        pyautogui.keyDown('w')   
         
+        if coordinates[0] != '' and coordinates[1] != '':
+            moveToDestination(desX, desY, coordinates[0], coordinates[1])
 
-        if float(destinationX) - float(x) < float(destinationX) - float(newX):
-            pyautogui.keyDown('d')
-            time.sleep(0.1)
-            pyautogui.keyUp('d')
+        coordinates = getCurrentCoordinates()
 
-        if float(destinationY) - float(y) < float(destinationY) - float(newY):
-            pyautogui.keyDown('d')
-            time.sleep(0.1)
-            pyautogui.keyUp('d')
+oldDX = 0
+oldDY = 0
+oldLength = 0
+
+def moveToDestination(desX, desY, x, y):
+    global oldDX
+    global oldDY
+    global oldLength
+
+    dx = desX - x
+    dy = desY - y
+
+    length = np.sqrt(dx*dx+dy*dy)
+
+    # dx /= length
+    # dy /= length
+    print(oldDX, oldDY, oldLength)
+    print(dx, dy, length)
+
+    if(oldDX > dx or oldDY > dy or oldLength > length or dx < 0 or dy < 0 or length < 0):
+        width, height = pyautogui.size()
+        print(width, height)
+        pyautogui.moveTo(width/2, height/2)
+        pyautogui.dragTo(width/2 + 10, height/2 - 45, 0.5, button='right')
+    
+
+    oldDX = dx
+    oldDY = dy
+    oldLength = length
+
+    # the length must always got to zero.
+
+
+
 
 
 
 time.sleep(4)
-moveTo(50.00, 32.00)
+moveTo(49.99, 49.99)
 
-# def walkTo(posX, posY):
-    # make a new screenshot
-    # get the coordinates of the screenshot
-    # check if destination is reaced
-
-
-#     # start walking in a random direction
-#     # get the coordinates and check if you are walking into the right direction
-#     # if the coordinates are futher adway from the target posX and posY, change direction using the a,s,w,d keys
-#     # repeat until the destination has been reachedwww
-
-# def clickNode():
-#     # when the player has arrived at a certain coordinate
-#     # try to detect the color of the node within a screenshot
-#     # the area with the most dense matching pixel colors is the node
-#     # click the node
-#     # make a chat screenshot
-#     # check if the player has looted new ores.
