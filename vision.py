c9d2acf3208d57ff0cae7e8684b5c12356da8367
Melvin Tehubijuluw <melvindrachten@hotmail.com>
2019-08-01 12:21:21 +0200

    bot can now turn right or left depending on the direction of the destination

--- vision.py
+++ vision.py
@@ -13,14 +13,12 @@ from pynput.mouse import Button, Controller
 
 width, heigth = pyautogui.size()
 mouse = Controller()
-oldDX = 0
-oldDY = 0
-oldLength = 0
+
 
 def makeCoordinateImage():
     #coordinates voor thuis laptop: 0, 34, 180, 25
-    coordinateImg = pyautogui.screenshot(region=(0, 34, 180, 25))
-    coordinateImg.save(r"C:/Users/Melvi/Desktop/shadowbot/coordinates.png")
+    coordinateImg = pyautogui.screenshot(region=(0, 25, 160, 25))
+    coordinateImg.save(r"C:/Users/Melvin Tehubijuluw/Desktop/shadowbot/coordinates.png")
     return coordinateImg
 
 
@@ -86,41 +84,125 @@ def inRangeOfDestination(desX, desY, x, y):
 
     return abs(desX - x) < 1 and abs(desY - y) < 1
 
+oldDX = 0
+oldDY = 0
+oldLength = 0
+oldX = 0
+oldY = 0
+
+moveKeysLookUp = {
+    "topLeft":  {
+        "topRight": "right",
+        "bottomRight": "right",
+        "bottomLeft": "left",
+        "topLeft": "stay",
+    },
+    "topRight":  {
+        "topLeft": "left",
+        "bottomRight": "right",
+        "bottomLeft": "right",
+        "topRight": "stay",
+    },
+    "bottomRight":  {
+        "topLeft": "left",
+        "topRight": "left",
+        "bottomLeft": "right",
+        "bottomRight": "stay",
+    },
+    "bottomLeft":  {
+        "topLeft": "right",
+        "topRight": "right",
+        "bottomRight": "left",
+        "bottomLeft": "stay",
+    },
+}
+
+
 def moveToDestination(desX, desY, x, y):
     global oldDX
     global oldDY
     global oldLength
     global mouse
+    global oldX
+    global oldY
+    global moveKeysLookUp
+
+    # Get the directions 
+
+    currentDirection = getDirection(x, y, oldX, oldY)
+    destinationDirection = getDirection(x, y, desX, desY)
+    turnKey = moveKeysLookUp[destinationDirection][currentDirection]
+
+    print('the current Direction', currentDirection)
+    print('the destination direction is', destinationDirection)
+    print('tuning to ', turnKey)
 
     dx = desX - x
     dy = desY - y
 
     length = np.sqrt(dx*dx+dy*dy)
 
-    # dx /= length
-    # dy /= length
-
-
-
-
     if(oldLength < length or (oldDX < dx and dx > 0) or (oldDY < dy and dy > 0)):
         width, height = pyautogui.size()
         pyautogui.keyUp('w')
         
         mouse.position = (width/2, height/2)
+
+        # click the correct mouse button
+        
         mouse.press(Button.right)
+      
         # TODO: when destination is very close, change the direction less
         amountOfPixelsToTurn = 20
         for i in range(amountOfPixelsToTurn): 
-            mouse.move(i, 0)
+            if turnKey == 'right':
+                mouse.move(i, 0)            
+            elif turnKey == 'left': 
+                mouse.move(-i, 0)
+            else:
+                mouse.move(0, 0)
+
             time.sleep(0.05)
+
+        # release the correct mouse button 
+    
         mouse.release(Button.right)
+      
+
         pyautogui.keyDown('w')
        
 
     oldDX = dx
     oldDY = dy
     oldLength = length
+
+
+def getDirection(x, y, oldX, oldY):
+    xIncreasing = True
+    yIncreasing = True
+
+    if (x - oldX) > 0:
+        xIncreasing = True 
+    else: 
+        xIncreasing = False
+
+    if (y - oldY) > 0:
+        yIncreasing = True
+    else:
+        yIncreasing = False
+
+
+    # Find the current direction the player is moving to
+    if xIncreasing and yIncreasing:
+        return "bottomRight"
+    elif xIncreasing and not(yIncreasing):
+        return "topRight"
+    elif not(xIncreasing) and not(yIncreasing):
+        return "topLeft"
+    elif not(xIncreasing) and yIncreasing:
+        return "bottomLeft"
+
+
 
 time.sleep(4)
 moveTo(30.07, 35.75)
@@ -130,6 +212,15 @@ moveTo(30.07, 35.75)
 # if x is increasing and y is increasing direction = bottom right of the map
 # if x is decreasing and y is decreasing direction = top left of the map
 # if x is decreasing and y is increasing direction = bottom left of the map
+
+
 # find out in what direction the destination is and use that to move the player to the left or right
+    # is the direction topLeft, topRight, bottomLeft, bottomRight
+    # is the player moveing to the topLeft, topRight, bottomLeft, bottomRight?
+
+
+
+
 # use the green/yellow circles on the minimap to check if a node is present in the target location
+
 
